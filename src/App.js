46aae6ddcbb2d76235399d437163bde8dc7c69e6
misterbean01 <rowanatkinson23@yahoo.com>
2022-01-26 19:49:35 -0800

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './components/HomeComponent'
+import { Traveller } from './components/TravellerComponent'
+import { Review } from './components/ReviewComponent'
+import { Location } from './components/LocationComponent'
+import { Navigation } from './components/NavigationComponent'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <h3 className="m-3 d-flex justify-content-center">
+          Welcome to Travel App
+        </h3>
+
+        <Navigation />
+
+        <Routes>
+          <Route path='/location' element={<Location />} />
+          <Route path='/traveller' element={<Traveller />} />
+          <Route path='/location/:locId/review' element={<Review />} />
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
